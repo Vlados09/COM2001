68fc0cb8d80d84170e18aff051c2a0f5f792f128
Vlad Bondarenko <Lucker@Vlads-MBP.local>
2017-10-18 15:13:55 +0100

    3 new functions

--- Assignment1/dominoes.hs
+++ Assignment1/dominoes.hs
@@ -1,12 +1,17 @@
 module Dominoes where
 
-    type Domino = [(Int, Int)]
+    type Domino = [Int, Int]
 
     dcreate :: Domino
     dcreate = []
 
     ddefine :: Int->Int -> Domino
-    ddefine a b = [(a,b)]
+    ddefine a b = [a,b]
+
+    dsame :: Domino->Domino -> Bool
+    dsame d1 d2
+      | (fst d1 == fst d2) && (snd d1 == snd d2) = True
+      | otherwise = False
 
     type Hand = [(Domino)]
 
@@ -21,16 +26,29 @@ module Dominoes where
     bcreate :: Board
     bcreate = []
 
-    type End = [(Char)]
+    type End = [Char]
 
     ecreate :: End
     ecreate = []
 
     edefine :: Char -> End
-    edefine c = [(c)]
+    edefine c = [c]
 
-    goesP :: Domino -> Board -> End ->Bool
+    goesP :: Domino->Board->End -> Bool
+    goesP _ [] _ = True
     goesP d b e
-        | null b = True
         | (e == L) = ((fst d == fst head b) || (snd d == fst head b))
         | (e == R) = ((fst d == last tail b) || (snd d == last tail b))
+
+    knockingP :: Hand->Board -> Bool
+    knockingP _ [] = True
+    knockingP [] _ = False
+    knockingP (h:t) b
+        | (goesP h b L) || (goesP h b R) = True
+        | otherwise knockingP t b
+
+    playedP :: Domino->Board -> Bool
+    playedP _ [] = False
+    playedP d (h:t)
+      | (dsame d h) = True
+      | otherwise playedP d t
