ae9485b5228db6cc79bef5642a0112911f071365
Vlados09 <vbondarenko1@sheffield.ac.uk>
2017-10-18 09:26:43 +0100

    completed first function

--- Assignment1/dominoes.hs
+++ Assignment1/dominoes.hs
@@ -1,8 +1,36 @@
 module Dominoes where
 
-  type Domino = [(Int, Int)]
-  type Hand = [(Domino)]
-  type Board = [(Domino)]
-  type End = [(Char)]
+    type Domino = [(Int, Int)]
 
-goesP :: Domino -> Board -> End ->Bool
+    dcreate :: Domino
+    dcreate = []
+
+    ddefine :: Int->Int -> Domino
+    ddefine a b = [(a,b)]
+
+    type Hand = [(Domino)]
+
+    hcreate :: Hand
+    hcreate = []
+
+    hput :: Domino->Hand -> Hand
+    hput d h = h : d
+
+    type Board = [(Domino)]
+
+    bcreate :: Board
+    bcreate = []
+
+    type End = [(Char)]
+
+    ecreate :: End
+    ecreate = []
+
+    edefine :: Char -> End
+    edefine c = [(c)]
+
+    goesP :: Domino -> Board -> End ->Bool
+    goesP d b e
+        | null b = True
+        | (e == L) = ((fst d == fst head b) || (snd d == fst head b))
+        | (e == R) = ((fst d == last tail b) || (snd d == last tail b))
