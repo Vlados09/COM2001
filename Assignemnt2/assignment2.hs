d141e61a946fb90cdeeb61c2ce2033f02a6c8281
Vlados09 <vbondarenko1@sheffield.ac.uk>
2017-11-02 17:42:58 +0000

    shuffleDoms function

--- Assignemnt2/assignment2.hs
+++ Assignemnt2/assignment2.hs
@@ -1,7 +1,15 @@
-import Dominoes
-
 module PlayDominoes where
+
+    import Dominoes
+    import System.Random
+
+    type Player = (Hand, Int)
+
     simplerPlayer :: DomsPlayer
     hsdPlayer :: DomsPlayer
 
-    DomsPlayer :: Hand->Board ->[Domino, End]
+    --DomsPlayer :: Hand->Board ->[Domino, End]
+
+    shuffleDoms :: Int -> Hand
+    shuffleDoms n = mergersort (\(_,n1) (_,n2)->n1<n2) (zip alld rlis)
+        where rlis = take 26 (randoms (mkStdGen n):: [Int])
